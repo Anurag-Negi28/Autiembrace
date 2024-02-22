4155ceebd39cff4a9ae7ce6de9f1fa4939fb313a
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-02-22 19:55:06 +0530

    Movie recommendation and emotion recognition section added

--- src/pages/AutiEmbrace.js
+++ src/pages/AutiEmbrace.js
@@ -3,8 +3,10 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import React, { useState, useEffect } from "react";
 import Todo from "../components/Todo";
+import { TextField, InputAdornment, Icon, IconButton } from "@mui/material";
 
 const AutiEmbrace = () => {
+  const [rectangleInputValue, setRectangleInputValue] = useState("");
   const navigate = useNavigate();
   const [quote, setQuote] = useState("");
 
@@ -14,14 +16,14 @@ const AutiEmbrace = () => {
 
   const fetchRandomQuote = async () => {
     try {
-      const response = await fetch('http://localhost:8080/api/quotes/random');
+      const response = await fetch("http://localhost:8080/api/quotes/random");
       if (!response.ok) {
-        throw new Error('Failed to fetch quote');
+        throw new Error("Failed to fetch quote");
       }
       const data = await response.json();
       setQuote(data.quote_text);
     } catch (error) {
-      console.error('Error fetching quote:', error);
+      console.error("Error fetching quote:", error);
     }
   };
 
@@ -58,99 +60,185 @@ const AutiEmbrace = () => {
           <b className="absolute w-[71.97%] top-[0px] left-[17.12%] leading-[40px] flex items-center justify-center h-[415px]">
             {quote}
           </b>
-          <i className="absolute w-[26.36%] top-[317px] left-[53.03%] text-base leading-[31px] inline-block font-montserrat h-[30px]">
-            
-          </i>
+          <i className="absolute w-[26.36%] top-[317px] left-[53.03%] text-base leading-[31px] inline-block font-montserrat h-[30px]"></i>
         </div>
       </main>
 
       <div className="relative mt-60 pl-50 pr-30 tododiv rounded-lg overflow-hidden">
         <Todo />
       </div>
-      
-      <div className="absolute w-[93.06%] top-[2133px] right-[3.47%] left-[3.47%] h-[914px] text-dimgray">
+
+      <div className="absolute top-[2143px] left-[50px] w-[1345px] h-[431px] text-11xl text-dimgray">
+        <div className="absolute top-[0px] left-[0px] rounded-31xl [background:linear-gradient(98.62deg,_rgba(255,_120,_164,_0.83),_rgba(255,_206,_206,_0)_80.1%)] w-[1340px] h-[431px]" />
         <img
-          className="absolute w-[49.33%] top-[0px] right-[0%] left-[50.67%] rounded-31xl max-w-full overflow-hidden h-[470px] object-cover"
+          className="absolute top-[0px] right-[0px] rounded-31xl w-[435px] h-[431px] object-cover"
           alt=""
-          src="/medalimg@2x.png"
+          src="/movieimg@2x.png"
         />
-        <div className="absolute w-[calc(100%_-_680px)] top-[0px] right-[680px] left-[0px] rounded-31xl bg-powderblue-100 h-[908px]" />
-        <b className="absolute w-[calc(100%_-_874px)] top-[87px] left-[76px] leading-[60px] inline-block [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]">
-          Rewards
-        </b>
-        <div className="absolute w-[35.45%] top-[180px] left-[5.37%] text-31xl leading-[50px] flex items-center h-[631px]">
-          <span className="w-full">
-            <p className="[margin-block-start:0] [margin-block-end:15px]">
-              Congratulations you have completed your day 1 tasks.
-            </p>
-          </span>
+        <div className="absolute w-[calc(100%_-_555px)] top-[29px] right-[483px] left-[72px] h-[113px]">
+          <h3 className="m-0 absolute w-[calc(100%_-_4px)] top-[77px] left-[4px] text-inherit leading-[20px] font-normal font-inherit inline-block h-9">
+            Try our movie recommender, Chill out and have fun
+          </h3>
+          <h1 className="m-0 absolute w-[calc(100%_-_13px)] top-[0px] left-[0px] text-41xl leading-[50px] font-bold font-inherit inline-block h-[61px]">
+            Done with tasks!!
+          </h1>
         </div>
-        <div className="absolute w-[calc(100%_-_678px)] top-[483px] right-[0px] left-[678px] rounded-31xl bg-powderblue-100 h-[431px]" />
-        <div className="absolute w-[30.3%] top-[622px] left-[69.7%] text-31xl leading-[50px] flex items-center h-[243px]">
-          Wash your hands before eating food.
+        <div className="absolute top-[155px] right-[509px] w-[367px] h-[257px]">
+          <h3 className="m-0 absolute top-[0px] right-[0px] text-inherit leading-[36px] font-normal font-inherit inline-block w-[367px] h-[46px]">
+            Suggestions 😊
+          </h3>
+          <textarea
+            className="[border:none] bg-gainsboro h-[184px] [outline:none] absolute top-[73px] right-[0px] rounded-xl w-[367px]"
+            placeholder="    Your movie suggestions are here!!"
+            rows={10}
+          />
         </div>
-        <b className="absolute w-[calc(100%_-_797px)] top-[539px] left-[764px] leading-[60px] inline-block [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]">
-          Norms Navigator
-        </b>
+        <div className="absolute top-[153px] left-[72px] w-[367px] h-[202px]">
+          <h3 className="m-0 absolute top-[0px] left-[4px] text-inherit leading-[36px] font-normal font-inherit flex items-center w-[363px] h-[66px]">
+            Enter your favorite movie name 🎥
+          </h3>
+          <input
+            className="[border:none] [outline:none] bg-gainsboro absolute top-[75px] left-[0px] rounded-xl w-[367px] h-[67px]"
+            placeholder="     Type movie name"
+            type="text"
+            value={rectangleInputValue}
+            onChange={(event) => setRectangleInputValue(event.target.value)}
+          />
+          <button className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[167px] left-[210px] w-[157px] h-[35px]">
+            <div className="absolute top-[0px] left-[0px] rounded-xl bg-powderblue-100 w-[157px] h-[35px]" />
+            <b className="absolute top-[0px] left-[0px] text-xl leading-[27.5px] flex font-montserrat text-dimgray text-center items-center justify-center w-[157px] h-[35px]">{`Generate>>`}</b>
+          </button>
+        </div>
         <img
-          className="absolute w-[14.78%] top-[636px] right-[32.76%] left-[52.46%] max-w-full overflow-hidden h-[215px] object-cover"
+          className="absolute w-[calc(100%_-_435px)] top-[140px] right-[435px] left-[0px] max-w-full overflow-hidden h-[5px] object-contain"
           alt=""
-          src="/phoneicon@2x.png"
+          src="/Line-5.png"
         />
       </div>
       <div className="absolute w-[99.93%] right-[0.07%] bottom-[0px] left-[0%] h-[431px] text-3xl">
-        <div className="absolute w-full right-[0%] bottom-[50px] left-[0%] [background:linear-gradient(180deg,_#b8c0d5,_rgba(193,_184,_213,_0))] [filter:blur(0px)] h-[381px]" />
-        <button
-          className="cursor-pointer [border:none] p-0 bg-[transparent] absolute right-[121px] bottom-[76px] text-3xl font-montserrat text-black text-left inline-block w-[101px] h-[27px]"
-          autoFocus={true}
-        >
+        <footer className="absolute w-full right-[0%] bottom-[50px] left-[0%] [background:linear-gradient(180deg,_#b8c0d5,_rgba(193,_184,_213,_0))] [filter:blur(0px)] h-[381px]" />
+        <button className="cursor-pointer [border:none] p-0 bg-[transparent] absolute right-[125px] bottom-[192px] text-3xl font-montserrat text-black text-left inline-block w-[101px] h-[27px]">
           Submit
         </button>
-        <div className="absolute w-[32.38%] bottom-[361px] left-[59.21%] inline-block">
+        <h3 className="m-0 absolute w-[32.38%] bottom-[361px] left-[59.21%] text-inherit font-normal font-inherit inline-block">
           Want to share? Give us feedback
-        </div>
+        </h3>
         <div className="absolute top-[92px] left-[365px] flex flex-col items-start justify-start gap-[2px] text-base">
-          <a className="[text-decoration:none] w-[141px] relative leading-[31px] text-[inherit] inline-block">
+          <a
+            className="[text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[141px]"
+            href="mailto:autiembrace@gmail.com"
+          >
             Email
           </a>
-          <a className="[text-decoration:none] w-[141px] relative leading-[31px] text-[inherit] inline-block">
+          <a
+            className="[text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[141px]"
+            href="https://linkedin.com"
+          >
             LinkedIn
           </a>
-          <a className="[text-decoration:none] w-[141px] relative leading-[31px] text-[inherit] inline-block">
+          <a
+            className="[text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[141px]"
+            href="https://instagram.com"
+          >
             Instagram
           </a>
-          <a className="[text-decoration:none] w-[141px] relative leading-[31px] text-[inherit] inline-block">
+          <a
+            className="[text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[141px]"
+            href="https://twitter.com"
+          >
             Twitter
           </a>
         </div>
-        <div className="absolute w-[9.8%] bottom-[361px] left-[25.36%] inline-block">
+        <h3 className="m-0 absolute w-[9.8%] bottom-[361px] left-[25.36%] text-inherit font-normal font-inherit inline-block">
           Contact
-        </div>
-        <div className="absolute bottom-[361px] left-[122px] inline-block w-[223px]">
+        </h3>
+        <h3 className="m-0 absolute bottom-[361px] left-[122px] text-inherit font-normal font-inherit inline-block w-[223px]">
           AutiEmbrace
-        </div>
+        </h3>
         <div className="absolute top-[92px] left-[122px] flex flex-col items-start justify-start gap-[2px] text-base">
-          <a className="[text-decoration:none] w-[121px] relative leading-[31px] text-[inherit] inline-block">
+          <a className="[text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[121px]">
             Home
           </a>
-          <a className="[text-decoration:none] w-[121px] relative leading-[31px] text-[inherit] inline-block">
+          <a className="[text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[121px]">
             About
           </a>
           <Link
-            className="cursor-pointer [text-decoration:none] w-[121px] relative leading-[31px] text-[inherit] inline-block"
-            to="/"
+            className="cursor-pointer [text-decoration:none] relative leading-[31px] text-[inherit] inline-block w-[121px]"
+            to="/landingpage"
             onClick={onSignOutClick}
           >
             Sign out
           </Link>
         </div>
-        <textarea className="[border:none] bg-white [outline:none] absolute w-[calc(100%_-_973px)] right-[123px] bottom-[133px] left-[850px] rounded-31xl h-48" />
+        <TextField
+          className="[border:none] bg-[transparent] absolute w-[calc(100%_-_973px)] right-[123px] bottom-[245px] left-[850px]"
+          color="primary"
+          placeholder="Your feedback matters!!"
+          sx={{ width: 466 }}
+          variant="outlined"
+        />
         <div className="absolute w-full right-[0px] bottom-[0px] left-[0px] bg-gainsboro h-[50px]" />
-        <div className="absolute top-[calc(50%+_165.5px)] left-[calc(50%-_111.5px)] whitespace-pre-wrap text-center flex items-center w-56 h-[50px]">
+        <div className="absolute top-[calc(50%_+_165.5px)] left-[calc(50%_-_111.5px)] whitespace-pre-wrap text-center flex items-center w-56 h-[50px]">
           <span className="w-full">
             <span>{`©  `}</span>
             <span className="text-lg">AutiEmbrace</span>
           </span>
+        </div>
+      </div>
+      <div className="absolute w-[calc(100%_+_260px)] top-[2590px] right-[-310px] left-[50px] h-[542px] text-21xl text-dimgray">
+        <div className="absolute w-[calc(100%_-_1129px)] top-[26px] right-[1129px] left-[0px] h-[431px]">
+          <div className="absolute w-full top-[0px] right-[0px] left-[0px] rounded-31xl [background:linear-gradient(30.11deg,_#b8d5cb,_rgba(244,_255,_248,_0))] h-[431px]" />
+          <h3 className="m-0 absolute w-[60.77%] top-[87px] left-[37.48%] text-inherit leading-[50px] font-normal font-inherit flex items-center h-[344px]">
+            Wash your hands before eating food.
+          </h3>
+          <h1 className="m-0 absolute w-[calc(100%_-_33px)] top-[27px] left-[23px] text-41xl leading-[60px] font-bold font-inherit inline-block [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]">
+            Norms Navigator
+          </h1>
+          <img
+            className="absolute w-[35.73%] top-[154px] right-[64.27%] left-[0%] max-w-full overflow-hidden h-[202px] object-cover"
+            alt=""
+            src="/phoneicon@2x.png"
+          />
+        </div>
+        <div className="absolute w-[calc(100%_-_593px)] top-[0px] right-[0px] left-[593px] h-[542px]">
+          <div className="absolute w-[calc(100%_-_360px)] top-[26px] right-[360px] left-[0px] rounded-31xl [background:linear-gradient(77.91deg,_#ff78a4,_rgba(184,_213,_195,_0))] h-[431px]" />
+          <h3 className="m-0 absolute w-[39.75%] top-[132px] left-[3.43%] text-inherit leading-[50px] font-normal font-inherit inline-block h-[325px]">
+            <p className="[margin-block-start:0] [margin-block-end:15px]">
+              😊😡😉
+            </p>
+            <p className="[margin-block-start:0] [margin-block-end:15px]">
+              Can you guess
+            </p>
+            <p className="[margin-block-start:0] [margin-block-end:15px]">
+              what we feel?
+            </p>
+            <p className="[margin-block-start:0] [margin-block-end:15px]">
+              Our software can,
+            </p>
+            <p className="[margin-block-start:0] [margin-block-end:15px]">
+              it's so real
+            </p>
+            <p className="[margin-block-start:0] [margin-block-end:15px]"></p>
+          </h3>
+          <h1 className="m-0 absolute w-[calc(100%_-_751px)] top-[47px] left-[38px] text-41xl leading-[60px] font-bold font-inherit flex items-center h-[72px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]">
+            EmoSense
+          </h1>
+          <div className="absolute top-[0px] right-[0px] w-[918px] h-[542px]">
+            <img
+              className="absolute top-[0px] right-[0px] w-[918px] h-[542px] object-cover mix-blend-soft-light"
+              alt=""
+              src="/blastimgae@2x.png"
+            />
+            <img
+              className="absolute top-[75px] right-[370.6px] rounded-150xl w-[305.4px] h-[291.2px] object-cover"
+              alt=""
+              src="/emotionrecognition@2x.png"
+            />
+          </div>
+          <button className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[392px] right-[458px] w-[131px] h-[39px]">
+            <div className="absolute top-[0px] right-[0px] rounded-xl bg-powderblue-100 w-[131px] h-[39px]" />
+            <b className="absolute top-[0px] right-[0px] text-xl leading-[27.5px] flex font-montserrat text-dimgray text-center items-center justify-center w-[131px] h-[39px]">{`Try it >>`}</b>
+          </button>
         </div>
       </div>
     </div>
